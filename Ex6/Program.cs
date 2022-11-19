eee1077e0289bafbc3491e2e3244399485a99021
Iurikman <y.m.kutyrev@gmail.com>
2022-11-19 14:34:02 +0300

    ex6 commit

---
+++ Ex6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Your name?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("HI, I love you!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
